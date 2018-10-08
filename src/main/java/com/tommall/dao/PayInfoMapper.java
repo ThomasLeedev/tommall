42ee4f52eb6d9d39654e82c37ca2b0d0de8460bf
thomasleedev <thomaslee.dev@gmail.com>
2018-10-08 14:20:05 +0800

    project init commit

---
+++ src/main/java/com/tommall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.tommall.dao;
+
+import com.tommall.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
---
+++ src/main/java/com/tommall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.tommall.dao;
+
+import com.tommall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
